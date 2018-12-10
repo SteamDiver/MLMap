d77ff27cb30a236c1e67bed69c44a75ea9a22901
Дмитрий Донских <steamdiver@yandex.ru>
2018-12-11 01:21:44 +0300

    added visual layer entry

---
+++ LogicObjects/Layer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicObjects
+{
+    public class Layer<T> where T:LayerObject
+    {
+        private List<T> LayerObjects { get; set; }
+    }
+}
---
+++ MLMap/VisualEntries/LayerEntry.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VisualEntries
+{
+    /// <summary>
+    /// Логика взаимодействия для LayerEntry.xaml
+    /// </summary>
+    public partial class LayerEntry : UserControl
+    {
+        public string Caption { get; set; }
+        public bool Visible { get; set; }
+        public LayerEntry()
+        {
+            InitializeComponent();
+        }
+    }
+}
